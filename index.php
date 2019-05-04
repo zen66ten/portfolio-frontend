be9d130628e4e2b9702a5713339d9981866fa679
zen66ten <37041718+zen66ten@users.noreply.github.com>
2019-05-04 20:49:09 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,177 @@
-<? php header( 'Location: /index.html' ) ; ?>
+<!DOCTYPE HTML>
+<!--
+	Dimension by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+-->
+<html>
+
+<head>
+	<title>Paweł Kraiński</title>
+	<meta charset="utf-8" />
+	<meta name="Description" content="Paweł Kraiński - Software Developer" />
+	<meta name="theme-color" content="#747574"/>
+	<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+	<link rel="stylesheet" href="assets/css/main.css" />
+	<link rel="shortcut icon" href="/favicon.png">
+	<noscript>
+		<link rel="stylesheet" href="assets/css/noscript.css" /></noscript>
+</head>
+
+<body class="is-preload">
+
+	<!-- Wrapper -->
+	<div id="wrapper">
+
+		<!-- Header -->
+		<header id="header">
+			<div class="content">
+				<div class="inner">
+					<h2>Paweł Kraiński</h2>
+					<p>Software developer</p>
+				</div>
+				<ul class="header-icons">
+					<li><a href="https://github.com/zen66ten" class="icon fa-github"><span class="label">GitHub</span></a></li>
+					<li><a href="https://www.linkedin.com/in/pawe%C5%82-krai%C5%84ski-16791a160/" class="icon fa-linkedin"><span class="label">LinkedIn</span></a></li>
+				</ul>
+			</div>
+			<nav>
+				<ul>
+					<li><a href="#intro">Skills</a></li>
+					<li id="portfolio"><a href="#work">Portfolio</a></li>
+					<li><a href="/assets/cv.pdf" download>Resume</a></li>
+					<li><a href="#contact">Contact</a></li>
+				</ul>
+			</nav>
+		</header>
+
+		<!-- Main -->
+		<div id="main">
+
+			<!-- Intro -->
+			<article id="intro">
+				<h2 class="major">Skills</h2>
+				<span class="image main"><img src="images/skills.webp" alt="" /></span>
+				<div class="skills-all">
+					<div class="skills-sections">
+						<h3>Technologies</h3>
+						<ul class="skills-list">
+							<li>HTML5,</li>
+							<li>CSS3,</li>
+							<li>Python,</li>
+							<li>JavaScript(ES5-ES8),</li>
+						</ul>
+					</div>
+					<div class="skills-sections">
+						<h3>Frameworks/Libraries</h3>
+						<ul class="skills-list">
+							<li>ReactJS,</li>
+							<li>NodeJS,</li>
+							<li>SASS,</li>
+							<li>Bootstrap,</li>
+							<li>Jest</li>
+						</ul>
+					</div>
+					<div class="skills-sections">
+						<h3>Backend/DevOps</h3>
+						<ul class="skills-list">
+							<li>PostgreSQL,</li>
+							<li>MongoDB,</li>
+							<li>Redis,</li>
+							<li>Docker,</li>
+							<li>Heroku,</li>
+							<li>AWS</li>
+						</ul>
+					</div>
+					<div class="skills-sections">
+						<h3>Tools</h3>
+						<ul class="skills-list">
+							<li>Visual Studio Code,</li>
+							<li>Git/Github,</li>
+							<li>SSH,</li>
+							<li>UNIX Shell</li>
+						</ul>
+					</div>
+					<div class="skills-sections">
+						<h3>Misc</h3>
+						<ul class="skills-list">
+							<li>Performance,</li>
+							<li>Security,</li>
+							<li>Unit/Integration Testing,</li>
+							<li>Caching,</li>
+							<li>Progressive Web Apps,</li>
+							<li>Data Structures,</li>
+							<li>Algorithms</li>
+						</ul>
+					</div>
+				</div>
+			</article>
+
+			<!-- Work -->
+			<article id="work">
+				<h2 class="major">Portfolio</h2>
+				<h3 class="center">Snake</h3>
+				<span class="image main"><img src="" alt="Snake" /></span>
+				<div class="app-buttons">
+					<a href="https://github.com/zen66ten/snake_curses_linux"><button class="view-app1">View App</button></a>
+					<a class="view-source" href="https://github.com/zen66ten/snake_curses_linux"><i class="icon fa-code"></i>View
+						Source Code</a>
+				</div>
+				<p class="description">A game of snake written in Python 3.7 that uses <b><i>curses</i></b> library
+					 which supplies a terminal-independent screen-painting and keyboard-handling facility for text-based terminals in Linux.</p>
+
+				<h3 class="center">TEST</h3>
+				<span class="image main"><img src="" alt="Weather Web-App" /></span>
+				<div class="app-buttons">
+					<a href="https://>.com"><button class="view-app3">View App</button></a>
+					<a class="view-source" href="https://>.com"><i class="icon fa-code"></i>View
+						Source Code</a>
+				</div>
+				<p class="description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam commodo velit sit amet tellus bibendum, vitae sollicitudin mauris aliquet. Etiam gravida bibendum arcu, quis aliquam massa aliquet vel.
+					 Pellentesque imperdiet tristique nisl semper aliquam. Nunc. .</p>
+
+				<h3 class="center">TEST</h3>
+				<span class="image main"><img src="" alt="" /></span>
+				<div class="app-buttons">
+					<a href="https://"><button class="view-app3">View App</button></a>
+					<a class="view-source" href="https://"><i class="icon fa-code"></i>View
+						Source Code</a>
+				</div>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam commodo velit sit amet tellus bibendum, vitae sollicitudin mauris aliquet. Etiam gravida bibendum arcu, quis aliquam massa aliquet vel.
+					 Pellentesque imperdiet tristique nisl semper aliquam. Nunc. </p>
+
+
+			</article>
+
+
+			<!-- Contact -->
+			<article id="contact">
+				<h2 class="major">Contact</h2>
+				<p class="email-link">Email: <a href="mailto:pe.krainski@gmail.com">pe.krainski@gmail.com</a></p>
+				<p>Phone: 697 068 991</p>
+				<ul class="icons">
+					<li><a href="https://github.com/zen66ten" class="icon fa-github"><span class="label">GitHub</span></a></li>
+					<li><a href="https://www.linkedin.com/in/pawe%C5%82-krai%C5%84ski-16791a160/" class="icon fa-linkedin"><span class="label">Instagram</span></a></li>
+				</ul>
+			</article>
+		</div>
+
+		<!-- Footer -->
+		<footer id="footer">
+		</footer>
+
+	</div>
+
+	<!-- BG -->
+	<div id="bg"></div>
+
+	<!-- Scripts -->
+	<script src="assets/js/jquery.min.js"></script>
+	<script src="assets/js/browser.min.js"></script>
+	<script src="assets/js/breakpoints.min.js"></script>
+	<script src="assets/js/util.js"></script>
+	<script src="assets/js/main.js"></script>
+
+</body>
+
+</html>
